0e21a93056e57463ec1cf4dae850ae9dfb3d6a45
sdras <sarah.drasner@gmail.com>
2018-02-04 10:01:55 -0700

    get everything working properly

--- service-worker.js
+++ service-worker.js
@@ -10,42 +10,31 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('sync', event => {
   let type = event.tag.split('-')[1]
-  // console.log(`sync event fired: request ${type}`)
-  // vm.syncInfo.fired = `sync event fired: request ${type}`
+  console.log(
+    `%c sync event fired: request ${type}`,
+    'font-size: 16px; background: #00449e; color: #cdecff; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+  )
   event.waitUntil(fetchResource(type))
 })
 
 const fetchResource = (resource, event) => {
-  console.log(`getting response info: ${resource}`)
+  console.log()
+  console.log(
+    `%c getting response info: ${resource}`,
+    'font-size: 16px; background: #5e2ca5; color: #d3b9f3; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+  )
   fetch('//jsonplaceholder.typicode.com/users')
-    .then(function(response) {
-      return response.status
+    .then(response => {
+      return response
     })
-    .then(function(text) {
-      console.log('Request successful, status:', text)
+    .then(text => {
+      console.log(
+        '%c Request successful:',
+        'font-size: 16px; background: #137752; color: #9eebcf; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+      )
+      console.log(text)
     })
-    .catch(function(error) {
+    .catch(error => {
       console.log('Request failed', error)
     })
 }
-
-self.addEventListener('sync', event => {
-  event.waitUntil(
-    (async function() {
-      // Exit early if we don't have access to the client.
-      // Eg, if it's cross-origin.
-      if (!event.clientId) return
-
-      // Get the client.
-      const client = await clients.get(event.clientId)
-      // Exit early if we don't get the client.
-      // Eg, if it closed.
-      if (!client) return
-
-      // Send a message to the client.
-      client.postMessage({
-        msg: 'Hey I just got a fetch from you!'
-      })
-    })()
-  )
-})
