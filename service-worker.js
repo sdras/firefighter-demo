17d839a7e4b0006222a245190900a08c1dc966f7
sdras <sarah.drasner@gmail.com>
2018-02-04 10:40:19 -0700

    code clean up

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,6 @@
+const styles =
+  'font-size: 16px; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+
 self.addEventListener('install', event => {
   self.skipWaiting()
 })
@@ -12,16 +15,15 @@ self.addEventListener('sync', event => {
   let type = event.tag.split('-')[1]
   console.log(
     `%c sync event fired: request ${type}`,
-    'font-size: 16px; background: #00449e; color: #cdecff; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+    `${styles} background: #00449e; color: #cdecff;`
   )
   event.waitUntil(fetchResource(type))
 })
 
-const fetchResource = (resource, event) => {
-  console.log()
+const fetchResource = resource => {
   console.log(
     `%c getting response info: ${resource}`,
-    'font-size: 16px; background: #5e2ca5; color: #d3b9f3; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+    `${styles} background: #5e2ca5; color: #d3b9f3;`
   )
   fetch('//jsonplaceholder.typicode.com/users')
     .then(response => {
@@ -30,7 +32,7 @@ const fetchResource = (resource, event) => {
     .then(text => {
       console.log(
         '%c Request successful:',
-        'font-size: 16px; background: #137752; color: #9eebcf; border-radius: 5px; padding: 5px 25px 5px 10px; line-height: 30px;'
+        `${styles} background: #137752; color: #9eebcf;`
       )
       console.log(text)
     })
