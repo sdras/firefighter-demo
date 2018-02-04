68f70ae42f4b44668da584f6c1e8fcc1f313af2d
sdras <sarah.drasner@gmail.com>
2018-02-04 02:00:22 -0700

    add in some css for the buttons

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,51 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   self.skipWaiting()
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   if (self.clients && clients.claim) {
     clients.claim()
   }
 })
 
-self.addEventListener('sync', function(event) {
-  var type = event.tag.split('-')[1]
-  console.log(`sync event fired: request ${type}`)
+self.addEventListener('sync', event => {
+  let type = event.tag.split('-')[1]
+  // console.log(`sync event fired: request ${type}`)
   // vm.syncInfo.fired = `sync event fired: request ${type}`
   event.waitUntil(fetchResource(type))
 })
 
-function fetchResource(resource) {
+const fetchResource = (resource, event) => {
   console.log(`getting response info: ${resource}`)
   fetch('//jsonplaceholder.typicode.com/users')
     .then(function(response) {
-      return response
+      return response.status
     })
     .then(function(text) {
-      console.log('Request successful', text)
+      console.log('Request successful, status:', text)
     })
     .catch(function(error) {
       console.log('Request failed', error)
     })
 }
+
+self.addEventListener('sync', event => {
+  event.waitUntil(
+    (async function() {
+      // Exit early if we don't have access to the client.
+      // Eg, if it's cross-origin.
+      if (!event.clientId) return
+
+      // Get the client.
+      const client = await clients.get(event.clientId)
+      // Exit early if we don't get the client.
+      // Eg, if it closed.
+      if (!client) return
+
+      // Send a message to the client.
+      client.postMessage({
+        msg: 'Hey I just got a fetch from you!'
+      })
+    })()
+  )
+})
